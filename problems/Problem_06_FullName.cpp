86ec430dd973f3d140992bab97b8e0ead4855f64
Asmaa Saci <imyasmaa3@gmail.com>
2025-10-28 18:08:47 +0100

    Update Problem_06_FullName.cpp

--- problems/Problem_06_FullName.cpp
+++ problems/Problem_06_FullName.cpp
@@ -29,3 +29,34 @@ int main() {
 	PrintName(GetFullName(ReadName()));
 	return 0;
 }
+
+///////////////////////////////
+//struct stName {
+//
+//	string FisrtName;
+//	string LastName;
+//
+//};
+//stName ReadName() {
+//	stName Name;
+//	cout << "Please enter your Fisrt Name ";
+//	cin >> Name.FisrtName;
+//	cout << "Please enter your Last Name ";
+//	cin >> Name.LastName;
+//	return Name;
+//}
+//string GetFullName(stName Name ,bool reversed) {
+//	string FullName = "";
+//	if (reversed)
+//	FullName = Name.LastName + " " + Name.FisrtName;
+//	else
+//	FullName = Name.FisrtName + " " + Name.LastName;
+//	return FullName;
+//};
+//void PrintName(string FullName) {
+//	cout << "\n Your FullName is : " << FullName << endl;
+//}
+//int main() {
+//	PrintName(GetFullName(ReadName(), true));
+//	return 0;
+//}
